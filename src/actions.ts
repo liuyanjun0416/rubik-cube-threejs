898a71b98c23b682fe5c5b674a43c6645153e750
Jun Hentai <kj65432123@gmail.com>
2019-01-28 22:43:00 +0800

    update 补全公式

--- src/actions.ts
+++ src/actions.ts
@@ -4,16 +4,20 @@ import { cubesObject } from "./object";
 const totalTime = 500;
 const config = {
   l: ["x", -50],
-  r: ["x", 50]
+  r: ["x", 50],
+  u: ["y", 50],
+  d: ["y", -50],
+  f: ["z",50],
+  b:["z",-50]
 };
 const halfCicle = (90 * Math.PI) / 180;
-const cubes = cubesObject.cubes;
+
 let isRotating = false;
 
 function filterCube(cubes: Mesh[], action: string) {
   return cubes.filter(cube => {
     return (
-      cube.position[config[action.toLocaleLowerCase()][0]] ===
+      Math.round(cube.position[config[action.toLocaleLowerCase()][0]]) ===
       config[action.toLocaleLowerCase()][1]
     );
   });
@@ -34,9 +38,9 @@ export function rotateAnimation(
     isRotating = false;
   }
   switch (action) {
-    case 'R':
+    case "R":
     case "l": {
-      filterCube(cubes, action).forEach(cube => {
+      filterCube(cubesObject.cubes, action).forEach(cube => {
         rotateAroundAxisX(
           cube,
           (-halfCicle * (currentTime - lastTime)) / totalTime
@@ -44,12 +48,52 @@ export function rotateAnimation(
       });
       break;
     }
-    case 'r':
+    case "r":
     case "L": {
-      filterCube(cubes, action).forEach(cube => {
+      filterCube(cubesObject.cubes, action).forEach(cube => {
         rotateAroundAxisX(
           cube,
           (halfCicle * (currentTime - lastTime)) / totalTime
+        );
+      });
+      break;
+    }
+    case "d":
+    case "U": {
+      filterCube(cubesObject.cubes, action).forEach(cube => {
+        rotateAroundAxisY(
+          cube,
+          (-halfCicle * (currentTime - lastTime)) / totalTime
+        );
+      });
+      break;
+    }
+    case "D":
+    case "u": {
+      filterCube(cubesObject.cubes, action).forEach(cube => {
+        rotateAroundAxisY(
+          cube,
+          (halfCicle * (currentTime - lastTime)) / totalTime
+        );
+      });
+      break;
+    }
+    case 'B':
+    case 'f':{
+      filterCube(cubesObject.cubes, action).forEach(cube => {
+        rotateAroundAxisZ(
+          cube,
+          (halfCicle * (currentTime - lastTime)) / totalTime
+        );
+      });
+      break;
+    }
+    case 'b':
+    case 'F':{
+      filterCube(cubesObject.cubes, action).forEach(cube => {
+        rotateAroundAxisZ(
+          cube,
+          (-halfCicle * (currentTime - lastTime)) / totalTime
         );
       });
       break;
@@ -71,7 +115,35 @@ export function rotateAroundAxisX(element: Mesh, rad: number) {
   element.position.z = Math.cos(rad) * z0 + Math.sin(rad) * y0;
 }
 
-export function rotateAroundAxisY(element: Mesh, rad: number) {}
+export function rotateAroundAxisY(element: Mesh, rad: number) {
+  const x0 = element.position.x;
+  const z0 = element.position.z;
+  let quaternion = new Quaternion();
+  quaternion.setFromAxisAngle(new Vector3(0, 1, 0), rad);
+  element.quaternion.premultiply(quaternion);
+  element.position.x = Math.cos(rad) * x0 + Math.sin(rad) * z0;
+  element.position.z = Math.cos(rad) * z0 - Math.sin(rad) * x0;
+}
+
+export function rotateAroundAxisZ(element: Mesh, rad: number) {
+  const x0 = element.position.x;
+  const y0 = element.position.y;
+  let quaternion = new Quaternion();
+  quaternion.setFromAxisAngle(new Vector3(0, 0, 1), rad);
+  element.quaternion.premultiply(quaternion);
+  element.position.x = Math.cos(rad) * x0 - Math.sin(rad) * y0;
+  element.position.y = Math.cos(rad) * y0 + Math.sin(rad) * x0;
+}
+
+function rotate(action: string) {
+  if (isRotating) {
+    return;
+  }
+  isRotating = true;
+  window.requestAnimationFrame(time => {
+    rotateAnimation(action, time, 0);
+  });
+}
 
 function handleEvent(action: string) {
   const dom = document.querySelector("." + action);
@@ -79,20 +151,14 @@ function handleEvent(action: string) {
     dom.addEventListener(
       "click",
       () => {
-        if (isRotating) {
-          return;
-        }
-        isRotating = true;
-        window.requestAnimationFrame(time => {
-          rotateAnimation(action, time, 0);
-        });
+        rotate(action);
       },
       false
     );
   }
 }
 
-const moveActionList = ["l", "L", "r", "R"];
+const moveActionList = ["l", "L", "r", "R", "u", "U", "d", "D","f","F","B","b"];
 export default function initAtions() {
   moveActionList.forEach(action => {
     handleEvent(action);
